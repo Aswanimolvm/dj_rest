3347193cc35b6e4e48c282ba584e61cd33dea8e4
Aswanimolvm <116870298+Aswanimolvm@users.noreply.github.com>
2025-05-03 12:56:14 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/items/';
+const API_URL = 'https://crybstocrayons.pythonanywhere.com/items/';
 
 const styles = {
   container: {
